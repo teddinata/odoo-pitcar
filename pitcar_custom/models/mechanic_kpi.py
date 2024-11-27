643769734c379ad891b84319e2ded31a44c7d318
teddinata <teddinata@users.noreply.github.com>
2024-11-27 13:59:01 +0700

    fix: bugfix emergency 3

--- pitcar_custom/models/mechanic_kpi.py
+++ pitcar_custom/models/mechanic_kpi.py
@@ -1,5 +1,8 @@
 from odoo import models, fields, api
 from datetime import datetime, timedelta
+import logging
+
+_logger = logging.getLogger(__name__)
 
 class MechanicKPI(models.Model):
     _name = 'mechanic.kpi'

42a26ff524c821768f7c0cba594d544cac2dd7bf
teddinata <teddinata@users.noreply.github.com>
2024-12-18 11:55:11 +0700

    fix: add quotation template service duration

---
+++ pitcar_custom/models/sale_order_template_line.py
@@ -0,0 +1,19 @@
+# models/sale_order_template_line.py
+from odoo import models, fields, api
+
+class SaleOrderTemplateLine(models.Model):
+    _inherit = "sale.order.template.line"
+
+    service_duration = fields.Float(
+        string='Durasi (Jam)',
+        help='Durasi estimasi layanan',
+        digits=(16, 2)
+    )
+
+    # models/sale_order_template_line.py
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        res = super()._onchange_product_id()
+        if self.product_id and self.product_id.type == 'service':
+            self.service_duration = self.product_id.service_duration
+        return res

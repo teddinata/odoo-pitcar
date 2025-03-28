b0e39aea68004330acd3ff3f783c8fe9164b4eb0
teddinata <teddinata@users.noreply.github.com>
2025-03-28 11:03:35 +0700

    fix add active column in mechanic table

--- pitcar_custom/models/res_partner.py
+++ pitcar_custom/models/res_partner.py
@@ -51,6 +51,8 @@ class PitcarMechanicNew(models.Model):
     
     def _get_default_color(self):
         return randint(1, 11)
+    
+    active = fields.Boolean(string='Active', default=True)
 
     name = fields.Char(string="Name", required=True, tracking=True)
     color = fields.Integer('Color', default=_get_default_color)

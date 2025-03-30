fba28936213f75ea852e2e1af8b81649cfd0e5b2
teddinata <teddinata@users.noreply.github.com>
2025-03-30 13:46:25 +0700

    fix add endpoint pdf export it

--- pitcar_custom/__manifest__.py
+++ pitcar_custom/__manifest__.py
@@ -64,6 +64,7 @@
         'report/report_cs_kpi.xml',
         'report/report_marketing_kpi.xml',
         'report/report_kaizen_kpi.xml',
+        'report/report_it_kpi.xml',
         'report/mechanic_kpi_reports.xml',
 
         'security/mechanic_security.xml',

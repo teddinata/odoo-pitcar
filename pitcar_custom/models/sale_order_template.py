94eb84c08a74ba9b72488883a486bebf7fcde9e5
teddinata <teddinata@users.noreply.github.com>
2025-05-19 15:45:27 +0700

    fix: add discount to template

--- pitcar_custom/models/sale_order_template.py
+++ pitcar_custom/models/sale_order_template.py
@@ -44,12 +44,15 @@ class SaleOrderTemplate(models.Model):
     def _compute_template_line_values(self, line, partner, pricelist, company, fiscal_position_id, date_order):
         # Memanggil fungsi asli dari Odoo
         result = super()._compute_template_line_values(line, partner, pricelist, company, fiscal_position_id, date_order)
-        
+                
         # Tambahkan harga kustom dari template line jika ada
         result['price_unit'] = line.price_unit or result.get('price_unit', 0.0)
         
+        # Tambahkan diskon dari template line
+        result['discount'] = line.discount
+                
         # Tambahkan service_duration jika ada
         if hasattr(line, 'service_duration'):
             result['service_duration'] = line.service_duration
-        
+                
         return result
